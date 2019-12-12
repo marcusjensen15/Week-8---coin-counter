3e93a8888a43619cfe7f21131299c24210325f34
Student <example@example.com>
2019-12-12 08:50:17 -0800

    add coin counter

---
+++ lib/project.rb
@@ -0,0 +1,22 @@
+class Coins
+  def initialize(input)
+    @input = input.to_i()
+    @quarters = 0
+    @dimes = 0
+    @nickels = 0
+    @pennies = 0
+  end
+
+def coin_counter
+ @quarters = (@input / 25).floor()
+  @input = @input - (@quarters * 25)
+  @dimes = (@input / 10).floor()
+  @input = @input - (@dimes * 10)
+  @nickels = (@input / 5).floor()
+  @input = @input - (@nickels * 5)
+  @pennies = @input
+  return "You have #{@quarters} quarters, #{@dimes} dimes, #{@nickels} nickels, #{@pennies} pennies."
+end
+
+
+end
--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -1,6 +1,10 @@
 require('rspec')
 require('project')
 
-describe ('#project') do
-  
+describe ('#Coins') do
+  it("should return the ammount of quarters for a given cent ammount")do
+    coins = Coins.new(115)
+    expect(coins.coin_counter).to(eq("You Have 4 Quarters"))
+  end
+
 end
